9b7cc6cb2e8025b0077e78c3c1747b9ace5d0427
meguvercin <m.guvercin34@gmail.com>
2025-04-13 03:32:38 +0300

    Refactor code structure and remove redundant changes

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -7,22 +7,30 @@ using DogusBlog.Models;
 namespace DogusBlog.Controllers
 {
 
-
     public class PostsController : Controller
     {
-        private IPostRepository _postRepository;
-        public PostsController(IPostRepository postRepository)
+
+        private IPostRepository _postRrepository;
+        public PostsController(IPostRepository postRrepository)
         {
-            _postRepository = postRepository;
+            _postRrepository = postRrepository;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index(string tag)
         {
-            return View(
-                new PostViewModel
-                {
-                    Posts = _postRepository.Posts.ToList(),
-                }
-            );
+            var claims = User.Claims;
+            var posts = _postRrepository.Posts;
+
+            if (!string.IsNullOrEmpty(tag))
+            {
+                posts = posts.Where(x => x.Tags.Any(t => t.Url == tag));
+            }
+            return View(new PostViewModel { Posts = await posts.ToListAsync() });
         }
+
+        public async Task<IActionResult> Details(string url)
+        {
+            return View(await _postRrepository.Posts.Include(x => x.Tags).Include(x => x.Comments).ThenInclude(x => x.User).FirstOrDefaultAsync(p => p.Url == url));
+        }
+
     }
 }
